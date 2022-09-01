5f2a2f639855aa90a86ea1cb267faf71a73199ab
PC <abdelrahmanmosa3d@gmail.com>
2022-09-01 11:17:13 +0200

    #3d895rw - generating a random 32 length hex token

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,21 @@
 class Application < ApplicationRecord
   has_many :chats
+  before_validation :generate_token, on: :create
 
   validates :name, presence: {:message => 'Application should have a name'}
   validates :token, presence: {:message => "Token field can't be blank"}
+
+  def generate_token
+    # This is the simpliest way to create a token, I thought that there is no
+    # need to use JWT token here.
+
+    # NOTE: there is a read (maybe more but the porbability is too low) from the
+    # database, it's not the best in the world, but judging from the use case,
+    # and that the creation requests on the application table is not that high,
+    # we can avoid complicating the token generation more than that.
+    self.token = loop do
+      generatedToken = SecureRandom.hex(32)
+      break generatedToken unless Application.exists?(token: generatedToken)
+    end
+  end
 end
