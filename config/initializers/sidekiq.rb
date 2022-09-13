4dfa3244a35beb83f8cb7d3433255fb3f4de9e20
PC <abdelrahmanmosa3d@gmail.com>
2022-09-13 09:45:38 +0200

    dockerizing sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,13 @@
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://redis:6379" }
+  print("----------------------- CONFIGURING SERVER")
+  config.redis = { url: "redis://redis:6379/0" }
 
   config.on(:startup) do
     Sidekiq::Cron::Job.load_from_hash YAML.load_file("config/schedule.yml")
-    Sidekiq::Scheduler.reload_schedule!
   end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://redis:6379" }
+  config.redis = { url: "redis://redis:6379/0" }
 end
